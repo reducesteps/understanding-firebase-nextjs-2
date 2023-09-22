9e90d1a6e27a95c76059167bcefd6ec587b0a7a2
ChatGPT <chatgpt@openai.com>
2023-09-22 17:39:54 +0000

    Deleted the replace_author_name.sh script.

--- replace_author_name.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# Replace all instances of 'Jonathan Oakey' with 'Jonathan David Oakey'
-find . -type f -name '*.md' -o -name '*.html' -exec sed -i 's/Jonathan Oakey/Jonathan David Oakey/g' {} +
